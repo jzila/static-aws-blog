3db0630ea9d4cd303500fc54a1084c744b7bfeee
John Zila <john@jzila.com>
2015-06-24 18:56:21 -0700

    Added support for only allowing my Keybase username

--- lambda/kbLoginVerify/kbLoginVerify.js
+++ lambda/kbLoginVerify/kbLoginVerify.js
@@ -4,12 +4,17 @@ exports.handler = function (event, context) {
     var blob = JSON.parse(event.blob);
     var signature = event.signature;
 
+    var user_auth_func = function(user) {
+        return user.kb_uid == '69da56f622a2ac750b8e590c3658a700';
+    };
+
     var keybaseSignin = new kblib.KeybaseSignin({
         AWS: {
             IdentityPoolId: 'us-east-1:0586c251-cf19-49d3-8a55-cfb1a4129b1b',
             IdentityId: event.identity_id,
             LoginProvider: "login.jzila.blog",
-            LambdaContext: context
+            LambdaContext: context,
+            UserAuthenticator: user_auth_func
         }
     });
     keybaseSignin.lookupKeybase(blob, signature);
